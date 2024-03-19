61492a53d8214c30080eabfe909ff792c097f27e
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-03-19 17:05:30 +0000

    update process_umbrella

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   erb(:home)
@@ -15,4 +16,23 @@ end
 
 get("/chat") do
   erb(:chat)
+end
+
+get("/process_umbrella") do
+  @user_location = params.fetch("user_loc")
+  gmaps_key = ENV.fetch("GMAPS_KEY")
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_key}"
+  gmaps_data = HTTP.get(gmaps_url).to_s
+  #gmaps_parse = JSON.parse(gmaps_data)
+
+
+  erb(:umbrella_results)
+end
+
+get("/process_message") do
+  erb(:process_message)
+end
+
+get("/add_message") do
+  erb(:add_message)
 end
