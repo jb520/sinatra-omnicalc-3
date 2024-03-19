8979dc28a83f948b8703cd543f3f7d3197023ea0
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-03-19 17:26:34 +0000

    debug process_umbrella

--- app.rb
+++ app.rb
@@ -18,21 +18,21 @@ get("/chat") do
   erb(:chat)
 end
 
-get("/process_umbrella") do
+post("/process_umbrella") do
   @user_location = params.fetch("user_loc")
   gmaps_key = ENV.fetch("GMAPS_KEY")
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_key}"
-  gmaps_data = HTTP.get(gmaps_url).to_s
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@user_location}&key=#{gmaps_key}"
+  @gmaps_data = HTTP.get(gmaps_url).to_s
   #gmaps_parse = JSON.parse(gmaps_data)
 
 
   erb(:umbrella_results)
 end
 
-get("/process_message") do
+post("/process_message") do
   erb(:process_message)
 end
 
-get("/add_message") do
+post("/add_message") do
   erb(:add_message)
 end
